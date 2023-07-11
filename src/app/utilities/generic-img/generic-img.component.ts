d4a76e57267b932a4f34344db54c36da77db9c3a
duvanceron <duvanceroon@hotmail.com>
2023-07-11 00:12:48 -0500

    Convert image in base64 an preview

--- src/app/utilities/generic-img/generic-img.component.ts
+++ src/app/utilities/generic-img/generic-img.component.ts
@@ -10,11 +10,11 @@ import { __values } from 'tslib';
 export class GenericImgComponent implements OnInit {
   imageBase64: string | any;
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+ 
   }
   constructor() {}
-  viewChanges(event: any) {
-    if (event.target.length > 0) {
+  viewChanges(event:any) {
+    if (event.target.files.length > 0) {
       const file: File = event.target.files[0];
       toBase64(file).then((valueF) => this.imageBase64 = valueF).catch(error => console.log(error));
     }
--- src/app/utilities/helper.ts
+++ src/app/utilities/helper.ts
@@ -14,7 +14,9 @@ export function toBase64(file:File) {
     return new Promise((resolve,reject)   => {
         const reader = new FileReader();
         reader.readAsDataURL(file);
+        console.log(reader.result);
         reader.onload = () => resolve(reader.result);   
+        console.log(reader.result);
         reader.onerror = error => reject(error);
     
     })
